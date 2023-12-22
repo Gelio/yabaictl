675a445e79eb5e6d08d2ad140f6d88f5c37f12c6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-12-22 12:17:41 +0100

    refactor(cli): enable info logs by default

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ enum Command {
 }
 
 fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let cli = Cli::parse();
 
